b0d765c59524d2ce5c8dbbeb5da801ce28c36493
quochiep1406 <quochiep1406@gmail.com>
2024-05-10 11:25:33 +0700

    feature/crud_category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,37 +1,32 @@
 @extends('admin.layouts.app')
-@section('title', 'Users')
+@section('title', 'Category')
 @section('content')
     <div class="card">
 
         @if (@session('message'))
             <h1 class="text-primary">{{ session('message') }}</h1>
         @endif
-        <h1>User List</h1>
+        <h1>Category List</h1>
         <div>
-            <a href="{{ route('users.create') }}" class="btn btn-primary">Create User</a>
+            <a href="{{ route('categories.create') }}" class="btn btn-primary">Create Category</a>
         </div>
 
         <div>
             <table class="table table-hover">
                 <tr>
                     <th>#</th>
-                    <th>Image</th>
                     <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
+                    <th>DisplayName</th>
                     <th>Action</th>
                 </tr>
-                @foreach ($users as $item)
+                @foreach ($categories as $item)
                     <tr>
                         <td>{{ $item->id }}</td>
-                        <th><img src="{{ $item->images->count() > 0 ? asset('upload/users/' . $item->images->first()->url) : 'upload/users/default.jpg' }}"
-                                width="200px" height="200px" alt=""></th>
                         <td>{{ $item->name }}</td>
-                        <td>{{ $item->email }}</td>
-                        <td>{{ $item->phone }}</td>
+                        <td>{{ $item->parent_name }}</td>
                         <td>
-                            <a href="{{ route('users.edit', $item->id) }}" class="btn btn-warning">Edit</a>
-                            <form action="{{ route('users.destroy', $item->id) }}" id="form-delete{{ $item->id }}"
+                            <a href="{{ route('categories.edit', $item->id) }}" class="btn btn-warning">Edit</a>
+                            <form action="{{ route('categories.destroy', $item->id) }} " id="form-delete{{ $item->id }}"
                                 method="post">
                                 @csrf
                                 @method('delete')
@@ -42,10 +37,9 @@
                     </tr>
                 @endforeach
             </table>
-            {{ $users->links() }}
+            {{ $categories->links() }}
         </div>
     </div>
-
 @endsection
 
 @section('scripts')
